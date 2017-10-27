b17a8e2cbe66125ef1bab4d1b02f43d59b834a07
PabloLeon <pleonvil@uos.de>
2017-10-27 10:31:07 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import MusicPlayer from "./MusicPlayer"
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+          <MusicPlayer />
+    );
+  }
+}
+
+export default App;
---
+++ src/MusicPlayer.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import {Container, Header, Image} from "semantic-ui-react";
+//import songexample from "songexample.mp3";
+
+
+class MusicPlayer extends Component {
+  state = {
+    title: "Mairi MacLean singing Se Tiriodh an t-eilean",
+    description: "",
+    source: "songexample.mp3",
+    artwork: "exampleImage.jpg",
+    currentTime: 0
+  }
+  render() {
+    return (
+    <Container>
+      <Image src={this.state.artwork} fluid/>
+        <Header>{this.state.title}</Header>
+        <audio src={"songexample.mp3"} autoPlay loop/>
+    </Container>
+    );
+  }
+}
+
+export default MusicPlayer;
