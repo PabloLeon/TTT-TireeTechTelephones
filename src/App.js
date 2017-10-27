65b162e8491a20b690b064279e158c63012b20b7
PabloLeon <pleonvil@uos.de>
2017-10-27 11:01:06 +0100

    added linting

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import MusicPlayer from "./MusicPlayer"
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import MusicPlayer from "./MusicPlayer";
+import "./App.css";
 
 class App extends Component {
-  render() {
-    return (
-          <MusicPlayer />
-    );
-  }
+	render() {
+		return <MusicPlayer />;
+	}
 }
 
 export default App;
--- src/MusicPlayer.js
+++ src/MusicPlayer.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
-import {Container, Header, Image} from "semantic-ui-react";
+import React, { Component } from "react";
+import { Container, Header, Image } from "semantic-ui-react";
 //import songexample from "songexample.mp3";
 
-
 class MusicPlayer extends Component {
-  state = {
-    title: "Mairi MacLean singing Se Tiriodh an t-eilean",
-    description: "",
-    source: "songexample.mp3",
-    artwork: "exampleImage.jpg",
-    currentTime: 0
-  }
-  render() {
-    return (
-    <Container>
-      <Image src={this.state.artwork} fluid/>
-        <Header>{this.state.title}</Header>
-        <audio src={"songexample.mp3"} autoPlay loop/>
-    </Container>
-    );
-  }
+	state = {
+		title: "Mairi MacLean singing Se Tiriodh an t-eilean",
+		description: "",
+		source: "songexample.mp3",
+		artwork: "exampleImage.jpg",
+		currentTime: 0
+	};
+	render() {
+		return (
+			<Container>
+				<Image src={this.state.artwork} fluid />
+				<Header>{this.state.title}</Header>
+				<audio src={"songexample.mp3"} autoPlay loop />
+			</Container>
+		);
+	}
 }
 
 export default MusicPlayer;
